2c9edad0a118a6777001f37727e966ab6e7980d9
Esteban-ExpenseTracker <rei.nhard3367@gmail.com>
2025-02-19 16:56:12 +0800

    updated codes -main

--- resources/js/Pages/Account/Account.vue
+++ resources/js/Pages/Account/Account.vue
@@ -7,50 +7,49 @@ import Cropper from "@/Components/Cropper.vue";
 import Authentication from "../Login/Authentication.vue";
 
 export default {
-    layout: Layout,
-    props: {
-        personalDetails: Object,
-        user: Object,
-        authDetails: Object,
-        auth: Boolean,
-        roles: Object,
-        authError: String,
+  layout: Layout,
+  props: {
+    personalDetails: Object,
+    user: Object,
+    authDetails: Object,
+    auth: Boolean,
+    roles: Object,
+    authError: String,
+  },
+  data() {
+    return {
+      activeTab: localStorage.getItem("activeTab") || "overview",
+      showModal: false,
+      userCredentials: {
+        user: "",
+        email: "",
+        password: "",
+      },
+    };
+  },
+  components: {
+    Overview,
+    PersonalDetails,
+    Security,
+    Cropper,
+    Authentication,
+  },
+  methods: {
+    setActiveTab(tab) {
+      this.activeTab = tab;
+      localStorage.setItem("activeTab", tab);
     },
-    data() {
-        return {
-            activeTab: localStorage.getItem("activeTab") || "overview",
-            showModal: false,
-            userCredentials: {
-                user: "",
-                email: "",
-                password: "",
-            },
-        };
+    openImageCropper() {
+      this.showModal = true;
     },
-    components: {
-        Overview,
-        PersonalDetails,
-        Security,
-        Cropper,
-        Authentication,
+    closeModal() {
+      this.showModal = false;
     },
-    methods: {
-        setActiveTab(tab) {
-            this.activeTab = tab;
-            localStorage.setItem("activeTab", tab);
-        },
-        openImageCropper() {
-            this.showModal = true;
-        },
-        closeModal() {
-            this.showModal = false;
-        },
-    },
+  },
 };
 </script>
 
 <template>
-
   <Cropper :showModal="showModal" @close="closeModal" />
   <nav>
     <span
@@ -91,6 +90,7 @@ export default {
           />
           <i @click="openImageCropper" class="edit-btn fas fa-camera"></i>
         </div>
+
         <div class="user-details">
           <span class="name">{{ user.first_name }} {{ user.last_name }}</span>
           <div class="user-role">
@@ -109,194 +109,190 @@ export default {
             <i class="fas fa-user-cog"></i>
             <div>
               <span class="role-desc">{{ roles.join(", ") }} </span>
-
             </div>
+          </div>
         </div>
-        <Authentication
-            :authDetails="authDetails"
-            :authError="authError"
-            v-if="!auth"
-        />
-
-        <section id="section" :class="!auth ? 'blur' : 'none-blur'">
-            <div v-if="activeTab === 'overview'">
-                <Overview :personalDetails="personalDetails" />
-            </div>
-
-            <div
-                v-if="activeTab === 'personalDetails'"
-                class="personal-details-container"
-            >
-                <PersonalDetails :personalDetails="personalDetails" />
-            </div>
-
-            <div class="security" v-if="activeTab === 'security'">
-                <Security :user="user" :userCredentials="userCredentials" />
-            </div>
-        </section>
+      </div>
     </div>
+
+    <Authentication :authDetails="authDetails" :authError="authError" v-if="!auth" />
+
+    <section id="section" :class="!auth ? 'blur' : 'none-blur'">
+      <div v-if="activeTab === 'overview'">
+        <Overview :personalDetails="personalDetails" />
+      </div>
+
+      <div v-if="activeTab === 'personalDetails'" class="personal-details-container">
+        <PersonalDetails :personalDetails="personalDetails" />
+      </div>
+
+      <div class="security" v-if="activeTab === 'security'">
+        <Security :user="user" :userCredentials="userCredentials" />
+      </div>
+    </section>
+  </div>
 </template>
 
 <style scoped>
 #section {
-    user-select: none;
+  user-select: none;
 }
 .blur {
-    filter: blur(5px);
+  filter: blur(5px);
 }
 @media (orientation: landscape) {
-    nav {
-        display: flex;
-        justify-content: start;
-        margin: 5px;
-        border-radius: 5px;
-        cursor: pointer;
-    }
-    nav span {
-        text-align: center;
-        font-weight: bold;
-        z-index: 10;
-        padding-top: 10px;
-        padding-bottom: 10px;
-        opacity: 40%;
-        color: #fff;
-        margin-left: 3rem;
-    }
+  nav {
+    display: flex;
+    justify-content: start;
+    margin: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  nav span {
+    text-align: center;
+    font-weight: bold;
+    z-index: 10;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    opacity: 40%;
+    color: #fff;
+    margin-left: 3rem;
+  }
 
-    nav span:hover {
-        opacity: 100%;
-        scale: 1.3;
-        transition-duration: 600ms;
-    }
+  nav span:hover {
+    opacity: 100%;
+    scale: 1.3;
+    transition-duration: 600ms;
+  }
 
-    nav i {
-        filter: drop-shadow(0px 4px 8px rgba(0, 0, 0, 0.3));
-    }
-    nav span.active {
-        color: #fff;
-        opacity: 100%;
-        scale: 1.3;
-    }
+  nav i {
+    filter: drop-shadow(0px 4px 8px rgba(0, 0, 0, 0.3));
+  }
+  nav span.active {
+    color: #fff;
+    opacity: 100%;
+    scale: 1.3;
+  }
 }
 
 @media (orientation: portrait) {
-    nav {
-        display: flex;
-        justify-content: space-around;
-        margin: 5px;
-        border-radius: 5px;
-    }
-    nav span {
-        text-align: center;
-        font-weight: bold;
-        z-index: 10;
-        padding-top: 10px;
-        padding-bottom: 10px;
-        opacity: 40%;
-        color: #fff;
-    }
+  nav {
+    display: flex;
+    justify-content: space-around;
+    margin: 5px;
+    border-radius: 5px;
+  }
+  nav span {
+    text-align: center;
+    font-weight: bold;
+    z-index: 10;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    opacity: 40%;
+    color: #fff;
+  }
 
-    nav span:hover {
-        opacity: 100%;
-        scale: 1.2;
-    }
+  nav span:hover {
+    opacity: 100%;
+    scale: 1.2;
+  }
 
-    nav i {
-        filter: drop-shadow(0px 4px 8px rgba(0, 0, 0, 0.3));
-    }
-    nav span.active {
-        color: #fff;
-        opacity: 100%;
-        scale: 1.3;
-    }
+  nav i {
+    filter: drop-shadow(0px 4px 8px rgba(0, 0, 0, 0.3));
+  }
+  nav span.active {
+    color: #fff;
+    opacity: 100%;
+    scale: 1.3;
+  }
 }
 
 .user-content {
-    padding: 10px;
+  padding: 10px;
 }
 
 .user-role {
-    display: flex;
-    gap: 0.5rem;
+  display: flex;
+  gap: 0.5rem;
 }
 .user-details {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 .user-details .name {
-    font-size: 30px;
-    font-weight: bold;
+  font-size: 30px;
+  font-weight: bold;
 }
 
 .user-details h1 {
-    color: #fff;
+  color: #fff;
 }
 
 .user-details i {
-    color: #fff;
+  color: #fff;
 }
 
 .user-details span {
-    color: #fff;
+  color: #fff;
 }
 
 .content {
-    margin: 5px;
+  margin: 5px;
 }
 .security {
-    width: 100%;
-    height: auto;
+  width: 100%;
+  height: auto;
 }
 
 .count {
-    font-size: 16px;
+  font-size: 16px;
 }
 
 .profile-container {
-    display: flex;
+  display: flex;
 }
 
 .user {
-    border-radius: 5px;
-    background: linear-gradient(to top, rgb(0, 0, 0), rgba(0, 0, 0, 0.1));
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: end;
-    align-items: start;
-    height: 30vh;
-    box-shadow: inset 0 4px 6px rgba(0, 0, 0, 0.3);
+  border-radius: 5px;
+  background: linear-gradient(to top, rgb(0, 0, 0), rgba(0, 0, 0, 0.1));
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  align-items: start;
+  height: 30vh;
+  box-shadow: inset 0 4px 6px rgba(0, 0, 0, 0.3);
 }
 
 .user-img {
-    height: 100px;
-    width: 100px;
-    border-radius: 50%;
-    object-fit: fill;
-    border: #dadada 4px solid;
-    box-shadow: 1px 2px 10px rgb(0, 0, 0);
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+  object-fit: fill;
+  border: #dadada 4px solid;
+  box-shadow: 1px 2px 10px rgb(0, 0, 0);
 }
 .user-img:hover {
-    cursor: pointer;
-    opacity: 60%;
+  cursor: pointer;
+  opacity: 60%;
 }
 
 .edit-btn {
-    display: absolute;
-    position: relative;
-    right: 30px;
-    bottom: 12px;
-    z-index: 9;
-    color: #fff;
-    font-size: 12px;
-    background-color: rgb(55, 103, 246);
-    padding: 7px;
-    border-radius: 100%;
-    cursor: pointer;
-    box-shadow: 1px 2px 10px rgb(0, 0, 0);
+  display: absolute;
+  position: relative;
+  right: 30px;
+  bottom: 12px;
+  z-index: 9;
+  color: #fff;
+  font-size: 12px;
+  background-color: rgb(55, 103, 246);
+  padding: 7px;
+  border-radius: 100%;
+  cursor: pointer;
+  box-shadow: 1px 2px 10px rgb(0, 0, 0);
 }
 
 .edit-btn:hover {
-    background-color: rgb(29, 81, 236);
+  background-color: rgb(29, 81, 236);
 }
 </style>
